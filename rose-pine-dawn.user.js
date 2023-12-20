5b835d205e9e90ec57988d413dd7d896ee181883
jorismertz <35079666+jorismertz@users.noreply.github.com>
2023-12-20 23:54:28 +0100

    format & fix colors

--- rose-pine-dawn.user.js
+++ rose-pine-dawn.user.js
@@ -10,15 +10,32 @@
 // @grant       none
 // ==/UserScript==
 
-(function() {
-	'use strict';
+(function () {
+  "use strict";
 
-	const theme = [
-		'1=-1', 'at=-1', 'ao=-1', 'aq=-1', 'ak=-1', 'ax=-1', 'av=1', 'ap=-1', 'au=-1', 'ay=b', 'ae=-1', '18=1',
-    '7=faf4ed', 'j=faf4ed', '9=d7827e', 'x=286983', 'aa=907aa9', '8=1f1d2e', '21=faf4ed',
-	];
+  const theme = [
+    "1=-1",
+    "at=-1",
+    "ao=-1",
+    "aq=-1",
+    "ak=-1",
+    "ax=-1",
+    "av=1",
+    "ap=-1",
+    "au=-1",
+    "ay=b",
+    "ae=-1",
+    "18=1",
+    "7=faf4ed",
+    "j=faf4ed",
+    "9=d7827e",
+    "x=286983",
+    "aa=907aa9",
+    "8=1f1d2e",
+    "21=faf4ed",
+  ];
 
-	for (const item of theme) {
-		document.cookie = `${item}; max-age=126144000; samesite=lax; secure`;
-	}
+  for (const item of theme) {
+    document.cookie = `${item}; max-age=126144000; samesite=lax; secure`;
+  }
 })();
--- rose-pine.user.js
+++ rose-pine.user.js
@@ -10,15 +10,32 @@
 // @grant       none
 // ==/UserScript==
 
-(function() {
-	'use strict';
+(function () {
+  "use strict";
 
-	const theme = [
-		'1=-1', 'at=-1', 'ao=-1', 'aq=-1', 'ak=-1', 'ax=-1', 'av=1', 'ap=-1', 'au=-1', 'ay=b', 'ae=-1', '18=1',
-    '7=191724', 'j=191724', '9=9ccfd8', 'x=31748f', 'aa=c4a7e7', '8=e0def4', '21=191724',
-	];
+  const theme = [
+    "1=-1",
+    "at=-1",
+    "ao=-1",
+    "aq=-1",
+    "ak=-1",
+    "ax=-1",
+    "av=1",
+    "ap=-1",
+    "au=-1",
+    "ay=b",
+    "ae=-1",
+    "18=1",
+    "7=191724",
+    "j=191724",
+    "9=9ccfd8",
+    "x=31748f",
+    "aa=c4a7e7",
+    "8=e0def4",
+    "21=191724",
+  ];
 
-	for (const item of theme) {
-		document.cookie = `${item}; max-age=126144000; samesite=lax; secure`;
-	}
+  for (const item of theme) {
+    document.cookie = `${item}; max-age=126144000; samesite=lax; secure`;
+  }
 })();
